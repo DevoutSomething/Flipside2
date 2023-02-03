c54f689b8c2fcdacb7e08b593e29a9c7cd837a54
Archer Fox <archer.l.fox@gmail.com>
2023-02-03 09:45:07 -0800

    he cant rotate :)

--- Assets/anim/MrPlant/PlantGoUp.cs
+++ Assets/anim/MrPlant/PlantGoUp.cs
@@ -10,6 +10,8 @@ public class PlantGoUp : MonoBehaviour
     public float distance = 6f;
     [SerializeField] private bool attacking;
     public float timeNoTarget = .5f;
+    public bool isRotRight;
+    public bool isRotLeft;
     void Start()
     {
         attacking = false;
@@ -17,10 +19,49 @@ public class PlantGoUp : MonoBehaviour
     }
     private void Update()
     {
+        if (isRotRight)
+        {
+            RaycastHit2D raycastPlantRight = Physics2D.Raycast(transform.position, Vector2.right, distance, playerLayer);
+            if (raycastPlantRight.collider != null)
+            {
+                Vector2 plantStartPoint;
+                plantStartPoint = transform.position;
+                Debug.DrawRay(plantStartPoint, Vector2.right * raycastPlantRight.distance, Color.red);
+                if (!attacking)
+                {
+                    StartCoroutine(SendAttackUp());
+                }
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, Vector2.right * distance, Color.green);
+            }
+        }
+        if (isRotLeft)
+        {
+            RaycastHit2D raycastPlantLeft = Physics2D.Raycast(transform.position, Vector2.left, distance, playerLayer);
+            if (raycastPlantLeft.collider != null)
+            {
+                Vector2 plantStartPoint;
+                plantStartPoint = transform.position;
+                Debug.DrawRay(plantStartPoint, Vector2.left * raycastPlantLeft.distance, Color.red);
+                if (!attacking)
+                {
+                    StartCoroutine(SendAttackUp());
+                }
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, Vector2.left * distance, Color.green);
+            }
+        }
         RaycastHit2D raycastPlant = Physics2D.Raycast(transform.position, Vector2.up, distance, playerLayer);
         if(raycastPlant.collider != null)
         {
-            Debug.DrawRay(transform.position, Vector2.up * raycastPlant.distance, Color.red);
+            Vector2 plantStartPoint;
+            plantStartPoint = transform.position;
+            plantStartPoint.y -= 3;
+            Debug.DrawRay(plantStartPoint, Vector2.up * raycastPlant.distance, Color.red);
             if (!attacking)
             {
                 StartCoroutine(SendAttackUp());
